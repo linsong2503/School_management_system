96b540c8c77c9e3342335e844822319aab2b02e6
madcode2503 <lamhope25nh@gmail.com>
2025-10-15 23:31:39 +0700

    realign_code_layout

--- client/app/(components)/Forms/modalNewTeacher.tsx
+++ client/app/(components)/Forms/modalNewTeacher.tsx
@@ -140,14 +140,11 @@ const ModalNewTeacher = ({ isOpen, onClose }: Props) => {
         <CldUploadWidget uploadPreset="school">
           {({ open }) => {
             return (
-              <div
-                className={inputStyles}
-                onClick={() => open()}
-              >
-               <div className="flex items-center gap-2 justify-center cursor-pointer">
-                 <Upload />
-                <span>Upload an image</span>
-               </div>
+              <div className={inputStyles} onClick={() => open()}>
+                <div className="flex items-center gap-2 justify-center cursor-pointer">
+                  <Upload />
+                  <span>Upload an image</span>
+                </div>
               </div>
             );
           }}
